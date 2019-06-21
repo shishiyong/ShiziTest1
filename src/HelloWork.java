1159f872221194f4a67477905811b880f63f94b3
shizi <m15706596005_1@163.com>
2019-06-21 20:16:26 +0800

    初始化

---
+++ src/HelloWork.java
@@ -0,0 +1,8 @@
+public class HelloWork {
+
+    public static void main(String[] args) {
+        String a="1";
+          System.out.println("helloword======");
+          System.out.println("helloword======ok");
+    }
+}
